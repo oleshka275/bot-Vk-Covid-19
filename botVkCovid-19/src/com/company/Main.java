9ddabb0954146dfeb8dcabbce468bf632766995f
oleshka275 <48128592+oleshka275@users.noreply.github.com>
2020-08-10 11:47:32 +0300

    Update Main.java

--- botVkCovid-19/src/com/company/Main.java
+++ botVkCovid-19/src/com/company/Main.java
@@ -8,7 +8,7 @@ import com.petersamokhin.bots.sdk.objects.Message;
 public class Main extends Parser {
 	
 	public static void main(String args[]){
-		Group group = new Group(189762760, "230862fce32fe4fd64b00730eae5073688460df00a51c620f0abfe3fd7357fff2037f01defba5aea7ed81");
+		Group group = new Group(189762760,TOKEN);
 	    
 		group.onSimpleTextMessage(message ->
 		     {
